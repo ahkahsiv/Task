534741cd40fb8dd63c0ce4dd3834d1af0853921b
Ahkahsiv <vishakha.yadav1901@gmail.com>
2023-02-07 19:52:00 +0530

    upd

--- project_1/home/urls.py
+++ project_1/home/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.form),
     path('write/',views.write),
-    path('add_vish/',views.add_vish)
+    path('add_vish/',views.add_vish),
+    path('text/',views.vish_text),
+    path('text_pdf/',views.vishs_pdf),
+
 ]
--- project_1/home/views.py
+++ project_1/home/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import vish
 import csv
+import reportlab
+# Import PDF Stuff
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+
 
 
 def form(request):
@@ -36,4 +45,70 @@ def write(request):
 
 	return response
 
+
+def vish_text(request):
+	response = HttpResponse(content_type='text/plain')
+	response['Content-Disposition'] = 'attachment; filename=vishss.txt'
+	# Designate The Model
+	vish_data = vish.objects.all()
+
+	# Create blank list
+	lines = []
+	# Loop Thu and output
+	for vishs in vish_data:
+		lines.append(f'{vishs.name}\n{vishs.phone}\n{vishs.email}\n')
+
+	#lines = ["This is line 1\n", 
+	#"This is line 2\n",
+	#"This is line 3\n\n",
+	#"John Elder Is Awesome!\n"]
+
+	# Write To TextFile
+	response.writelines(lines)
+	return response
+
+
+
+# Generate a PDF File vishs List
+def vishs_pdf(request):
+	# Create Bytestream buffer
+	buf = io.BytesIO()
+	# Create a canvas
+	c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+	# Create a text object
+	textob = c.beginText()
+	textob.setTextOrigin(inch, inch)
+	textob.setFont("Helvetica", 14)
+
+	# Add some lines of text
+	#lines = [
+	#	"This is line 1",
+	#	"This is line 2",
+	#	"This is line 3",
+	#]
+	
+	# Designate The Model
+	vish_data = vish.objects.all()
+
+	# Create blank list
+	lines = []
+
+	for vishs in vish_data:
+		lines.append(vishs.name)
+		lines.append(vishs.phone)
+		lines.append(vishs.email)
+		lines.append(" ")
+
+	# Loop
+	for line in lines:
+		textob.textLine(line)
+
+	# Finish Up
+	c.drawText(textob)
+	c.showPage()
+	c.save()
+	buf.seek(0)
+
+	# Return something
+	return FileResponse(buf, as_attachment=True, filename='vishs.pdf')
 # Create your views here.
